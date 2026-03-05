4ec8df1d9ba8acb7b94d5b8910aee9e8750e7c3d
Kacey Dove <kjdove@udel.edu>
2026-03-05 09:17:06 -0500

    removed old function in insights file

--- src/reports/viewInsights.ts
+++ src/reports/viewInsights.ts
@@ -199,34 +199,7 @@ function updateButtonVisibility() {
     }
 }//end to updateButtonVisibility
 
-// function pastEvents() {
-//     const today = new Date().toISOString().split("T")[0];
-
-//     waitForCalendarEvents(() => {
-
-//         const todayCell = $(`.fc-day[data-date="${today}"]`);
-//         const weekRow = todayCell.closest(".fc-row");
-
-//         const pastDays = weekRow.find(".fc-day[data-date]").filter((_, el) => {
-//             const date = $(el).attr("data-date");
-//             return date! < today; 
-//         });
-
-//         pastDays.each((_, day) => {
-//             const columnIndex = $(day).index();
-
-//             const events = weekRow
-//                 .find(".fc-content-skeleton tbody tr")
-//                 .find(`td:eq(${columnIndex}) .fc-event`);
-
-//             events.css("opacity", "0.5");
-//         });
-
-//     });
-// }//end to pastEvents
-
 export function loadInsightsReport() {
     updateButtonVisibility();
     window.addEventListener('hashchange', updateButtonVisibility);
-    // pastEvents();
 }//end to loadInsightsReport
