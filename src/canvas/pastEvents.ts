1d6bb7a243adbb35b4c3999ff6c0314dcfc713b9
Kacey Dove <kjdove@udel.edu>
2026-03-05 09:35:07 -0500

    past months events are shaded

--- src/canvas/pastEvents.ts
+++ src/canvas/pastEvents.ts
@@ -7,26 +7,39 @@ import { waitForCalendarEvents } from "src/reports/viewInsights";
 
 export function shadePastEvents() {
     const today = new Date().toISOString().split("T")[0];
+    console.log('today', today);
+    const viewStart = window.location.hash.split("view_start=")[1]?.split("&")[0];
+    console.log('viewStart', viewStart);
 
-    //when calendar renders current month
-    waitForCalendarEvents(() => {
-        const todayCell = $(`.fc-day[data-date="${today}"]`);
-        const weekRow = todayCell.closest(".fc-row");
-
-        const pastDays = weekRow.find(".fc-day[data-date]").filter((_, el) => {
-            const date = $(el).attr("data-date");
-            return date! < today; 
+    //if calendar renders current month
+    if(viewStart === today){
+        waitForCalendarEvents(() => {
+            const todayCell = $(`.fc-day[data-date="${today}"]`);
+            const weekRow = todayCell.closest(".fc-row");
+    
+            const pastDays = weekRow.find(".fc-day[data-date]").filter((_, el) => {
+                const date = $(el).attr("data-date");
+                return date! < today; 
+            });
+    
+            pastDays.each((_, day) => {
+                const columnIndex = $(day).index();
+    
+                const events = weekRow
+                    .find(".fc-content-skeleton tbody tr")
+                    .find(`td:eq(${columnIndex}) .fc-event`);
+    
+                events.css("opacity", "0.5");
+            });
         });
-
-        pastDays.each((_, day) => {
-            const columnIndex = $(day).index();
-
-            const events = weekRow
-                .find(".fc-content-skeleton tbody tr")
-                .find(`td:eq(${columnIndex}) .fc-event`);
-
+    }
+    //else if calendar renders a past month, shade all events
+    else if(viewStart < today){
+        waitForCalendarEvents(() => {
+            const events = $(".fc-content-skeleton tbody tr").find(".fc-event");
             events.css("opacity", "0.5");
         });
-    });
+    }
+   
     window.addEventListener('hashchange', shadePastEvents);
 }//end to shadePastEvents
