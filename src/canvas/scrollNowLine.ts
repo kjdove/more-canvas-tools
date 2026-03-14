91591ec6d96563c60e3f8e1f70a96c3bc8638c2f
Kacey Dove <kjdove@udel.edu>
2026-03-13 21:03:51 -0400

    updated scrolling and hash change logic

--- src/canvas/scrollNowLine.ts
+++ src/canvas/scrollNowLine.ts
@@ -4,27 +4,44 @@
  */
 
 function scrollToNowLine() {
+    // const currentWeek = 
     const isWeekView =
-      window.location.pathname.includes('/calendar') &&
-      window.location.hash.includes('view_name=week');
+        window.location.pathname.includes('/calendar') &&
+        window.location.hash.includes('view_name=week');
+    if (!isWeekView) return;
     
-      if(isWeekView){
-        const nowline = $(`.calendar-nowline`);
-        // console.log('found nowline', nowline);
-        const scrollContent = $(`.fc-scroller.fc-time-grid-container`);
-        // console.log('found scroll content', scrollContent);
+    if(isWeekView){
+        setTimeout(() => {
+            const nowline = $(`.calendar-nowline`);
+            // console.log('found nowline', nowline.position());
+            // console.log('nowline ', nowline.get(0));    
+            const scrollContent = $(`.fc-scroller.fc-time-grid-container`);
+            // console.log('found scroll content', scrollContent);
 
-        if(nowline && scrollContent){
-            nowline.get(0)?.scrollIntoView({
-                behavior: "smooth",
-                block: "center"
-            });
-        }//end to if
-      }//end to if week view
+            if(nowline.length && scrollContent.length){
+                const offset = nowline.position()?.top ?? 0;
+                const scHeight = scrollContent.height() ?? 0;
+                scrollContent.animate({
+                    scrollTop: offset - scHeight / 2
+                    }, 400);
+            }//end to if
+        }, 300)//wait for the week view to be rendered
+    }//end to if week view
 
 }//end to scrollToNowLine
 
 export function loadScrollToNowLine() {
-    window.addEventListener('hashchange', scrollToNowLine);
+    scrollToNowLine();
     window.addEventListener('load', scrollToNowLine);
-}
+    let lastHash = window.location.hash;
+    const watchHashChange = () => {
+        setInterval(() => {
+            if (window.location.hash !== lastHash) {
+              lastHash = window.location.hash;
+              console.log("hash changed", lastHash);
+              scrollToNowLine();
+            }
+          }, 200);
+    }//end to watchHashChange
+    watchHashChange();
+}//end to loadScrollToNowLine
